b4c3fc028121d8e31b0b61b584d17907b11bacfb
Andy Lucas <andy@xmos.com>
2014-01-24 19:44:34 +0000

    Fix formatting of debug print.

--- module_avb_srp/src/avb_mrp.c
+++ module_avb_srp/src/avb_mrp.c
@@ -748,9 +748,9 @@ static void mrp_update_state(mrp_event e, mrp_attribute_state *st, int four_pack
 
 void mrp_debug_dump_attrs(void)
 {
-#if 0
-  debug_printf("port_num | type                   | disabled | here | propagated | stream_id\n \
-                ---------+------------------------+----------+------+------------+----------\n");
+#if 1
+  debug_printf("port_num | type                   | disabled | here | propagated | stream_id\n"
+                "---------+------------------------+----------+------+------------+----------\n");
   for (int i=0;i<MRP_MAX_ATTRS;i++) {
 
     if (attrs[i].applicant_state != MRP_UNUSED) {
