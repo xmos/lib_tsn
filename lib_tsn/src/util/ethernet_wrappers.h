d22b4c769ff240f20ba228f648ec80fc938e8d23
Andy Lucas <andy@xmos.com>
2014-11-11 10:01:12 +0000

    Missing xccompat include in ethernet wrappers header

--- lib_tsn/src/util/ethernet_wrappers.h
+++ lib_tsn/src/util/ethernet_wrappers.h
@@ -1,6 +1,7 @@
 #ifndef ETHERNET_WRAPPERS_H_
 #define ETHERNET_WRAPPERS_H_
 
+#include <xccompat.h>
 #include "ethernet.h"
 
 unsafe void eth_send_packet(CLIENT_INTERFACE(ethernet_if, i), char *unsafe packet, unsigned n,
