af197d8e94718db224ce827eac08292d93531375
Andy Lucas <andy@xmos.com>
2015-01-05 22:34:37 +0000

    Adds new AEM descriptor structs and control defines

--- lib_tsn/src/1722_1/aem_descriptor_structs.h
+++ lib_tsn/src/1722_1/aem_descriptor_structs.h
@@ -1,6 +1,103 @@
 // Copyright (c) 2015, XMOS Ltd, All rights reserved
 #include <inttypes.h>
 
+typedef struct aem_desc_audio_unit_t
+{
+    uint8_t descriptor_type[2];
+    uint8_t descriptor_index[2];
+    uint8_t object_name[64];
+    uint8_t localized_description[2];
+    uint8_t clock_domain_index[2];
+    uint8_t number_of_stream_input_ports[2];
+    uint8_t base_stream_input_port[2];
+    uint8_t number_of_stream_output_ports[2];
+    uint8_t base_stream_output_port[2];
+    uint8_t number_of_external_input_ports[2];
+    uint8_t base_external_input_port[2];
+    uint8_t number_of_external_output_ports[2];
+    uint8_t base_external_output_port[2];
+    uint8_t number_of_internal_input_ports[2];
+    uint8_t base_internal_input_port[2];
+    uint8_t number_of_internal_output_ports[2];
+    uint8_t base_internal_output_port[2];
+    uint8_t number_of_controls[2];
+    uint8_t base_control[2];
+    uint8_t number_of_signal_selectors[2];
+    uint8_t base_signal_selector[2];
+    uint8_t number_of_mixers[2];
+    uint8_t base_mixer[2];
+    uint8_t number_of_matrices[2];
+    uint8_t base_matrix[2];
+    uint8_t number_of_splitters[2];
+    uint8_t base_splitter[2];
+    uint8_t number_of_combiners[2];
+    uint8_t base_combiner[2];
+    uint8_t number_of_demultiplexers[2];
+    uint8_t base_demultiplexer[2];
+    uint8_t number_of_multiplexers[2];
+    uint8_t base_multiplexer[2];
+    uint8_t number_of_transcoders[2];
+    uint8_t base_transcoder[2];
+    uint8_t number_of_control_blocks[2];
+    uint8_t base_control_block[2];
+    uint8_t current_sampling_rate[4];
+    uint8_t sampling_rates_offset[2];
+    uint8_t sampling_rates_count[2];
+    // sampling_rates
+} aem_desc_audio_unit_t;
+
+typedef struct aem_desc_control_t
+{
+    uint8_t descriptor_type[2];
+    uint8_t descriptor_index[2];
+    uint8_t object_name[64];
+    uint8_t localized_description[2];
+    uint8_t block_latency[4];
+    uint8_t control_latency[4];
+    uint8_t control_domain[2];
+    uint8_t control_value_type[2];
+    uint8_t control_type[8];
+    uint8_t reset_time[4];
+    uint8_t values_offset[2];
+    uint8_t number_of_values[2];
+    uint8_t signal_type[2];
+    uint8_t signal_index[2];
+    uint8_t signal_output[2];
+    // value_details
+} aem_desc_control_t;
+
+typedef struct aem_desc_signal_selector_t
+{
+    uint8_t descriptor_type[2];
+    uint8_t descriptor_index[2];
+    uint8_t object_name[64];
+    uint8_t localized_description[2];
+    uint8_t block_latency[4];
+    uint8_t control_latency[4];
+    uint8_t control_domain[2];
+    uint8_t sources_offset[2];
+    uint8_t number_of_sources[2];
+    uint8_t current_signal_type[2];
+    uint8_t current_signal_index[2];
+    uint8_t current_signal_output[2];
+    uint8_t default_signal_type[2];
+    uint8_t default_signal_index[2];
+    uint8_t default_signal_output[2];
+    // sources
+} aem_desc_signal_selector_t;
+
+typedef struct aem_desc_clock_domain_t
+{
+    uint8_t descriptor_type[2];
+    uint8_t descriptor_index[2];
+    uint8_t object_name[64];
+    uint8_t localized_description[2];
+    uint8_t clock_source_index[2];
+    uint8_t clock_sources_offset[2];
+    uint8_t clock_sources_count[2];
+    // clock_sources
+} aem_desc_clock_domain_t;
+
 typedef struct aem_desc_audio_cluster_t {
     uint8_t descriptor_type[2];
     uint8_t descriptor_index[2];
@@ -35,9 +132,8 @@ typedef struct aem_desc_stream_input_output_t {
     uint8_t backedup_talker_unique_id[2];
     uint8_t avb_interface_index[2];
     uint8_t buffer_length[4];
-    #define MAX_NUM_STREAM_FORMATS 8
-    uint8_t formats[8*MAX_NUM_STREAM_FORMATS];
-    #undef  MAX_NUM_STREAM_FORMATS
+    #define MAX_NUM_STREAM_FORMATS 16
+    uint8_t formats[16*MAX_NUM_STREAM_FORMATS];
 } aem_desc_stream_input_output_t;
 
 typedef struct aem_audio_map_format_t {
@@ -54,7 +150,6 @@ typedef struct aem_desc_audio_map_t {
     uint8_t number_of_mappings[2];
     #define MAX_NUM_MAPPINGS 8
     aem_audio_map_format_t mappings[MAX_NUM_MAPPINGS];
-    #undef  MAX_NUM_MAPPINGS
 } aem_desc_audio_map_t;
 
 typedef struct aem_desc_stream_port_input_output_t {
