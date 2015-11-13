7e7f2ea63a8eb37f557dfb419d7e4e92dd1e2ada
Rob Fisher <robf@xmos.com>
2015-11-13 12:24:03 +0000

    Initial check in.  Working on GP slice Kit.

---
+++ examples/AN01032_100Mbit_avb_i2s_demo/src/debug_conf.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2015, XMOS Ltd, All rights reserved
+#ifndef DEBUG_CONF_H_
+#define DEBUG_CONF_H_
+
+#define DEBUG_PRINT_ENABLE      1
+#define DEBUG_PRINT_ENABLE_ALL 1
+#define DEBUG_PRINT_DISABLE_ETHERNET_CLIENT_HANDLER 1
+#define DEBUG_PRINT_DISABLE_ETHERNET_FILTER 1
+
+#endif /* DEBUG_CONF_H_ */
